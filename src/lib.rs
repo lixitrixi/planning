4b9f8cdca8825c1ef8d0d14b0f1245698e193e72
Felix <felix@felixleitner.com>
2024-08-04 18:58:17 -0400

    add agent type and update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -36,11 +36,17 @@ where
     /// Returns true if the goal is satisfied in the given state.
     fn is_satisfied(&self, state: &S) -> bool;
 
-    /// Returns a heuristic estimate of the cost to reach the goal from the given state.
-    /// The default implementation returns a constant, and implementing this may make the search more efficient.
+    /// Returns a heuristic estimate of the total cost to satisfy the goal from the given state.
+    /// The default implementation returns a constant, and implementing this will make the search more efficient.
     ///
-    /// When implementing, the heuristic should not *overestimate* the cost.
+    /// The heuristic should not *overestimate* the actual cost, or else resulting plans may be incorrect.
     fn heuristic(&self, state: &S) -> i32 {
+        0
+    }
+
+    /// Returns a priority for this goal based on the given state.
+    /// This may return a constant for each goal, or be implemented support a dynamic priority system.
+    fn priority(&self, state: &S) -> i32 {
         0
     }
 }
@@ -89,12 +95,57 @@ where
     }
 }
 
-/// Return a sequence of actions to reach the goal while minimising cost. Returns None if no plan is found.
+/// Returns a sequence of actions to reach the goal while minimizing cost, if possible.
+///
+/// # Example
+/// ```
+/// # use dyn_goap::*;
+///
+/// #[derive(PartialEq, Eq, Hash, Clone)]
+/// struct State {
+///     is_correct: bool,
+/// }
+///
+/// #[derive(PartialEq, Eq, Hash, Clone, Debug)]
+/// struct MakeCorrect;
+///
+/// impl Action<State> for MakeCorrect {
+///     fn is_applicable(&self, state: &State) -> bool {
+///         !state.is_correct
+///     }
+///
+///     fn apply_mut(&self, state: &mut State) {
+///         state.is_correct = true;
+///     }
+/// }
+///
+/// #[derive(PartialEq, Eq, Hash, Clone)]
+/// struct IsCorrect;
+///
+/// impl Goal<State> for IsCorrect {
+///     fn is_satisfied(&self, state: &State) -> bool {
+///         state.is_correct
+///     }
+/// }
+///
+/// let actions = vec![MakeCorrect];
+/// let goal = IsCorrect;
+///
+/// let initial_state = State { is_correct: false };
+/// let (path, cost) = plan(&initial_state, &actions, &goal).unwrap();
+/// assert_eq!(path, vec![MakeCorrect]);
+/// assert_eq!(cost, 1);
+///
+/// let initial_state = State { is_correct: true };
+/// let (path, cost) = plan(&initial_state, &actions, &goal).unwrap();
+/// assert_eq!(path, vec![]);
+/// assert_eq!(cost, 0);
+/// ```
 pub fn plan<S, A, G>(initial_state: &S, actions: &Vec<A>, goal: &G) -> Option<(Vec<A>, i32)>
 where
     S: Clone + Hash + Eq,
     A: Action<S> + Eq + Clone + Hash,
-    G: Goal<S>,
+    G: Goal<S> + Clone,
 {
     let initial = PlanNode {
         state: initial_state.clone(),
@@ -117,48 +168,132 @@ where
     })
 }
 
+#[cfg_attr(feature = "bevy", derive(bevy::prelude::Component))]
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Agent<S, A, G>
+where
+    S: Clone + Hash + Eq,
+    A: Action<S> + Eq + Clone + Hash,
+    G: Goal<S> + Clone,
+{
+    pub state: S,
+    pub actions: Vec<A>,
+    pub goals: Vec<G>,
+}
+
+impl<S, A, G> Agent<S, A, G>
+where
+    S: Clone + Hash + Eq,
+    A: Action<S> + Eq + Clone + Hash,
+    G: Goal<S> + Clone,
+{
+    /// Creates a new agent with the given initial state, possible actions, and goals.
+    ///
+    /// The goals are sorted by priority based on the initial state.
+    /// This implementation uses a stable sorting algorithm, so goals with the same priority will remain in the same order.
+    pub fn new(state: S, actions: Vec<A>, goals: Vec<G>) -> Self {
+        let mut goals = goals;
+        goals.sort_by(|a, b| b.priority(&state).cmp(&a.priority(&state))); // sort in reverse order
+        Self {
+            state,
+            actions,
+            goals,
+        }
+    }
+
+    /// Returns the plan and total cost for the first goal that can be satisfied.
+    ///
+    /// This method does not sort the goals by priority before searching.
+    /// This implementation uses a stable sorting algorithm, so goals with the same priority will remain in the same order.
+    ///
+    /// **If your goals return different priorities based on state, use `plan_dynamic` instead.**
+    pub fn plan_constant(&self) -> Option<(&G, Vec<A>, i32)> {
+        self.goals.iter().find_map(|goal| {
+            plan(&self.state, &self.actions, goal).map(|(path, cost)| (goal, path, cost))
+        })
+    }
+
+    /// Returns the plan and total cost for the first goal that can be satisfied.
+    ///
+    /// This method sorts the goals by priority based on the current state before searching.
+    /// This implementation uses a stable sorting algorithm, so goals with the same priority will remain in the same order.
+    ///
+    /// **If your goals return the same priority regardless of state, use `plan_constant` instead.**
+    ///
+    /// # Example
+    /// ```
+    /// # use dyn_goap::*;
+    ///
+    /// #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    /// struct State {
+    ///     has_worked: bool,
+    ///     hungry: bool,
+    /// }
+    ///
+    /// #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    /// enum MyAction {
+    ///     Work,
+    ///     Eat,
+    /// }
+    ///
+    /// impl Action<State> for MyAction {
+    ///     fn is_applicable(&self, state: &State) -> bool {
+    ///         true // both are always possible
+    ///     }
+    ///
+    ///     fn apply_mut(&self, state: &mut State) {
+    ///         match self {
+    ///             MyAction::Work => state.has_worked = true,
+    ///             MyAction::Eat => state.hungry = false,
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    /// enum MyGoal {
+    ///     Worked,
+    ///     Eaten,
+    /// }
+    ///
+    /// impl Goal<State> for MyGoal {
+    ///     fn is_satisfied(&self, state: &State) -> bool {
+    ///         match self {
+    ///             MyGoal::Worked => state.has_worked,
+    ///             MyGoal::Eaten => !state.hungry,
+    ///         }
+    ///     }
+    ///
+    ///     fn priority(&self, state: &State) -> i32 {
+    ///         match self {
+    ///             MyGoal::Worked => 1,
+    ///             MyGoal::Eaten => if state.hungry { 2 } else { 0 },
+    ///         }
+    ///     }
+    /// }
+    ///
+    /// let mut agent = Agent::new(
+    ///    State { has_worked: false, hungry: false },
+    ///    vec![MyAction::Work, MyAction::Eat],
+    ///    vec![MyGoal::Worked, MyGoal::Eaten],
+    /// );
+    ///
+    /// let (goal, _, _) = agent.plan_dynamic().unwrap();
+    /// assert_eq!(goal, &MyGoal::Worked);
+    ///
+    /// agent.state.hungry = true; // agent will now prioritize eating
+    /// let (goal, _, _) = agent.plan_dynamic().unwrap();
+    /// assert_eq!(goal, &MyGoal::Eaten);
+    /// ```
+    pub fn plan_dynamic(&mut self) -> Option<(&G, Vec<A>, i32)> {
+        self.goals
+            .sort_by(|a, b| b.priority(&self.state).cmp(&a.priority(&self.state)));
+        self.plan_constant()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn plan_single_step() {
-        #[derive(PartialEq, Eq, Hash, Clone)]
-        struct State {
-            is_correct: bool,
-        }
-
-        #[derive(PartialEq, Eq, Hash, Clone, Debug)]
-        struct MakeCorrect;
-
-        impl Action<State> for MakeCorrect {
-            fn is_applicable(&self, state: &State) -> bool {
-                !state.is_correct
-            }
-
-            fn apply_mut(&self, state: &mut State) {
-                state.is_correct = true;
-            }
-        }
-
-        #[derive(PartialEq, Eq, Hash, Clone)]
-        struct IsCorrect;
-
-        impl Goal<State> for IsCorrect {
-            fn is_satisfied(&self, state: &State) -> bool {
-                state.is_correct
-            }
-        }
-
-        let initial_state = State { is_correct: false };
-        let actions = vec![MakeCorrect];
-        let goal = IsCorrect;
-
-        let (path, cost) = plan(&initial_state, &actions, &goal).unwrap();
-        assert_eq!(path.len(), 1);
-        assert_eq!(cost, 1);
-        assert_eq!(path[0], MakeCorrect);
-    }
 
     #[test]
     fn plan_complex() {
